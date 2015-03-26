cda553050cab159f69e61e58cfcc7e260f79d20c
MCastro <MCastro@ManuelCT.mshome.net>
2015-03-26 17:22:50 -0600

    shopping now allows for purchases to be registered

--- app/database/seeds/RolesTableSeeder.php
+++ app/database/seeds/RolesTableSeeder.php
@@ -10,7 +10,9 @@ class RolesTableSeeder extends Seeder {
 
     public function run() {
         DB::table('roles')->insert(array(
-            array('id' => 1, 'description' => 'admin'))
+            array('id' => 1, 'description' => 'admin',
+               'created_at'=>"2015-03-23",
+               'updated_at'=>"2015-03-23",))
         );
     }
 
