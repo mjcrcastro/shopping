39f62895543f0990884a7f1cd380371bd38f8c81
MCastro <MCastro@ManuelCT.mshome.net>
2015-04-13 18:39:48 -0600

    added function to allow download of all descriptors

--- app/controllers/LoginController.php
+++ app/controllers/LoginController.php
@@ -37,7 +37,7 @@ class LoginController extends \BaseController {
 
             // attempt to do the login
             if (Auth::attempt($userdata)) {
-                return Redirect::to('/');
+                return redirect()->intended('/');
             } else {
                 // validation not successful, send back to form	
                 return Redirect::to('login')
