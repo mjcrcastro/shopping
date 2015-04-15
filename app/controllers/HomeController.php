5c4bef488bc6140ecbcbcfdf3feb3c1d0486002f
MCastro <MCastro@ManuelCT.mshome.net>
2015-04-15 07:49:02 -0600

    added function to allow download of all descriptors

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -15,12 +15,19 @@ class HomeController extends \BaseController {
     }
 
     public function getData() {
-        $allTables = DB::select('SHOW TABLES');
+
+        if (Config::get('database.default') === 'mysql') {
+            $allTables = DB::select('SHOW TABLES');
+        } else {
+            $allTables = DB::select("SELECT * FROM pg_catalog.pg_tables WHERE schemaname != 'pg_catalog' AND schemaname != 'information_schema'");
+        }
+        
+        return Helper::lastQuery();
 
         foreach ($allTables as $table) {
 
             $tableName = $table->Tables_in_lar_shopping;
-            
+
             DB::setFetchMode(PDO::FETCH_ASSOC);
             $result = DB::table($tableName)->get(); // array of arrays instead of objects
             DB::setFetchMode(PDO::FETCH_CLASS);
@@ -36,28 +43,28 @@ class HomeController extends \BaseController {
             }
 
             fclose($handle);
-
-            
         }
-        
+
         //now zip the files
         $zipFile = "backup.zip";
         $zipArchive = new ZipArchive();
 
-        if (!$zipArchive->open($zipFile, ZIPARCHIVE::OVERWRITE))
+        if (!$zipArchive->open($zipFile, ZIPARCHIVE::OVERWRITE)) {
             die("Failed to create archive\n");
+        }
 
-        $zipArchive->addGlob("*.csv");
-        if (!$zipArchive->status == ZIPARCHIVE::ER_OK)
+        $zipArchive->addGlob("*.csv"); 
+        if (!$zipArchive->status == ZIPARCHIVE::ER_OK) {
             echo "Failed to write files to zip\n";
+        }
 
         $zipArchive->close();
-        
-        
+
+
         $headers = array(
-                'Content-Type' => 'application/octet-stream',
-            );
-        
+            'Content-Type' => 'application/octet-stream',
+        );
+
         return Response::download($zipFile, $zipFile, $headers);
     }
 
