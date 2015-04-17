21075abbebd8db6a5a2e18ae3ca6b836f71c9b55
MCastro <MCastro@ManuelCT.mshome.net>
2015-04-16 18:04:15 -0600

    added function to allow download of all descriptors

--- app/controllers/PurchasesController.php
+++ app/controllers/PurchasesController.php
@@ -156,7 +156,7 @@ class PurchasesController extends \BaseController {
             return Redirect::route('purchases.index');
         }
         $shops = Shop::orderBy('description','asc')->lists('description', 'id');
-        $products_purchases = ProductPurchases::where('purchase_id','=',$purchase->id);
+        $products_purchases = ProductPurchase::where('purchase_id','=',$purchase->id);
         return View::make('purchases.edit', compact('purchase','shops','products_purchases'));
         // End of actual code to execute
     }
