f7493593c4bde5c5ac6d77b021aecff245a33106
MCastro <MCastro@ManuelCT.mshome.net>
2015-04-21 08:33:54 -0600

    users can now edit a purchase

--- app/controllers/PurchasesController.php
+++ app/controllers/PurchasesController.php
@@ -291,7 +291,7 @@ class PurchasesController extends \BaseController {
             return "GROUP_CONCAT(DISTINCT descriptors.description ORDER BY "
             . "descriptors.descriptorType_id SEPARATOR ' ') as product_description";
         } else {
-            return "array_to_string(array_agg(descriptors.description ORDER BY descriptors.description), ' ' "
+            return "array_to_string(array_agg(descriptors.description ORDER BY descriptors.descriptortype_id), ' ' "
             . ") as product_description ";
         }
     }
