4f41ddbed5be29c19961db419fbdcb6d749e9c5d
MCastro <MCastro@ManuelCT.mshome.net>
2015-04-21 08:32:01 -0600

    users can now edit a purchase

--- app/controllers/PurchasesController.php
+++ app/controllers/PurchasesController.php
@@ -288,9 +288,11 @@ class PurchasesController extends \BaseController {
 
     private function dbRaw() {
         if (Config::get('database.default') === 'mysql') {
-            return "GROUP_CONCAT(DISTINCT descriptors.description ORDER BY descriptors.descriptorType_id SEPARATOR ' ') as product_description";
+            return "GROUP_CONCAT(DISTINCT descriptors.description ORDER BY "
+            . "descriptors.descriptorType_id SEPARATOR ' ') as product_description";
         } else {
-            return "array_to_string(array_agg(descriptors.description), ' ' ORDER BY descriptors.description) as product_description ";
+            return "array_to_string(array_agg(descriptors.description ORDER BY descriptors.description), ' ' "
+            . ") as product_description ";
         }
     }
 
